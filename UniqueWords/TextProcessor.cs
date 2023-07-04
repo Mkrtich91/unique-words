16bafa4b605b817e799e37466e290828b622d825
Mkrtich Kazanchian <mkogeo@gmail.com>
2023-07-04 22:37:09 +0400

    solved at 04.07 mk

--- UniqueWords/TextProcessor.cs
+++ UniqueWords/TextProcessor.cs
@@ -1,4 +1,9 @@
-﻿namespace UniqueWords
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace UniqueWords
 {
     public static class TextProcessor
     {
@@ -7,9 +12,17 @@
         /// </summary>
         public static List<string> GetUniqueWordsFromArray(string[] words)
         {
-            // TODO #1. Analyze the unit tests for this method, and add a method implementation.
-            // Use List<T>.Contains and List<T>.Add methods.
-            throw new NotImplementedException();
+            var list = new List<string>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!list.Contains(words[i]))
+                {
+                    list.Add(words[i]);
+                }
+            }
+
+            return list;
         }
 
         /// <summary>
@@ -17,9 +30,21 @@
         /// </summary>
         public static int CountUniqueWordsInText(string text)
         {
-            // TODO #2. Analyze the unit tests for this method, and add a method implementation.
-            // Use List<T>.Contains, List<T>.Add, String.IndexOf method and ranges.
-            throw new NotImplementedException();
+            string[] words = text.Split(new char[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries)
+                         .Select(word => word.ToLower(CultureInfo.InvariantCulture))
+                         .ToArray();
+
+            List<string> uniqueWords = new List<string>();
+
+            foreach (string word in words)
+            {
+                if (!uniqueWords.Contains(word))
+                {
+                    uniqueWords.Add(word);
+                }
+            }
+
+            return uniqueWords.Count;
         }
 
         /// <summary>
@@ -27,9 +52,21 @@
         /// </summary>
         public static IEnumerable<string> GetUniqueWordsFromEnumerable(IEnumerable<string> lines)
         {
-            // TODO #3. Analyze the unit tests for this method, and add a method implementation.
-            // Use List<T>.Contains, List<T>.Add, String.Split method.
-            throw new NotImplementedException();
+            List<string> uniqueWords = new List<string>();
+
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(' ');
+
+                foreach (string word in words)
+                {
+                    if (!uniqueWords.Contains(word))
+                    {
+                        uniqueWords.Add(word);
+                        yield return word;
+                    }
+                }
+            }
         }
 
         /// <summary>
@@ -37,9 +74,38 @@
         /// </summary>
         public static string[][] GetUniqueWordsArray(IEnumerable<string> lines, char separator)
         {
-            // TODO #4. Analyze the unit tests for this method, and add a method implementation.
-            // Use List<T>.Contains, List<T>.Add, String.IndexOf, String.Substring and List<T>.ToArray() methods.
-            throw new NotImplementedException();
+            List<string[]> uniqueWordsList = new List<string[]>();
+
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+                List<string> uniqueWords = new List<string>();
+
+                foreach (string word in words)
+                {
+                    string trimmedWord = word.Trim();
+                    bool isDuplicate = false;
+
+                    foreach (string[] prevLineWords in uniqueWordsList)
+                    {
+                        if (prevLineWords.Contains(trimmedWord))
+                        {
+                            isDuplicate = true;
+                            break;
+                        }
+                    }
+
+                    if (!isDuplicate)
+                    {
+                        uniqueWords.Add(trimmedWord);
+                    }
+                }
+
+                uniqueWordsList.Add(uniqueWords.ToArray());
+            }
+
+            return uniqueWordsList.ToArray();
         }
 
         /// <summary>
@@ -47,9 +113,37 @@
         /// </summary>
         public static ICollection<string> GetUniqueWordsCollection(string text, char separator)
         {
-            // TODO #5. Analyze the unit tests for this method, and add a method implementation.
-            // Use List<T>.Contains, List<T>.Add, String.IndexOf method and ranges.
-            throw new NotImplementedException();
+            text = text.Replace("\r\n", " ", StringComparison.InvariantCulture).ToString(CultureInfo.InvariantCulture).Trim();
+            List<string> uniqueWords = new List<string>();
+            int startIndex = 0;
+
+            while (startIndex < text.Length)
+            {
+                int separatorIndex = text.IndexOf(separator, startIndex);
+                if (separatorIndex == -1)
+                {
+                    string lastWord = text[startIndex..].Trim();
+                    uniqueWords.Add(lastWord);
+                    break;
+                }
+                else
+                {
+                    string word = text[startIndex..separatorIndex].Trim();
+                    uniqueWords.Add(word);
+                    startIndex = separatorIndex + 1;
+                }
+            }
+
+            List<string> distinctWords = new List<string>();
+            foreach (string word in uniqueWords)
+            {
+                if (!distinctWords.Contains(word))
+                {
+                    distinctWords.Add(word);
+                }
+            }
+
+            return distinctWords;
         }
 
         /// <summary>
@@ -57,8 +151,36 @@
         /// </summary>
         public static ICollection<string> GetUniqueWordsInCharCollection(ICollection<char> collection, char separator)
         {
-            // TODO #6. Analyze the unit tests for this method, and add a method implementation.
-            throw new NotImplementedException();
+            List<string> uniqueWords = new List<string>();
+            StringBuilder wordBuilder = new StringBuilder();
+
+            foreach (char character in collection)
+            {
+                if (character == separator)
+                {
+                    string word = wordBuilder.ToString().Trim();
+
+                    if (!string.IsNullOrEmpty(word) && !uniqueWords.Contains(word))
+                    {
+                        uniqueWords.Add(word);
+                    }
+
+                    wordBuilder.Clear();
+                }
+                else
+                {
+                    wordBuilder.Append(character);
+                }
+            }
+
+            string lastWord = wordBuilder.ToString().Trim();
+
+            if (!string.IsNullOrEmpty(lastWord) && !uniqueWords.Contains(lastWord))
+            {
+                uniqueWords.Add(lastWord);
+            }
+
+            return uniqueWords;
         }
     }
 }
